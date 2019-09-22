242ba0eb663a0d9ee9f855fd7f1f67684fd68f69
Elliot Spaull <elliotspaull@gmail.com>
2019-09-22 10:51:21 +0100

    First solution

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -4,52 +4,69 @@ function Item(name, sell_in, quality) {
   this.quality = quality;
 }
 
-var items = []
+var items = [];
 
-function update_quality() {
-  for (var i = 0; i < items.length; i++) {
-    if (items[i].name != 'Aged Brie' && items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-      if (items[i].quality > 0) {
-        if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-          items[i].quality = items[i].quality - 1
-        }
-      }
+const itemTypes = {
+  conjured: { items: [], baseDecay: 2 },
+  legendary: { items: [], baseDecay: 0 },
+  cheese: { items: [], baseDecay: -1 },
+  pass: { items: [], baseDecay: -1 },
+  regular: { items: [], baseDecay: 1 },
+};
+
+const categoriseItems = () => {
+  for (const item of items) {
+    if (item.name.match(/conjured/gi) != null) {
+      itemTypes['conjured'].items.push(item);
+    } else if (item.name.match(/sulfuras/gi) != null) {
+      itemTypes['legendary'].items.push(item);
+    } else if (item.name.match(/brie/gi) != null) {
+      itemTypes['cheese'].items.push(item);
+    } else if (item.name.match(/passes/gi) != null) {
+      itemTypes['pass'].items.push(item);
     } else {
-      if (items[i].quality < 50) {
-        items[i].quality = items[i].quality + 1
-        if (items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].sell_in < 11) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-          if (items[i].sell_in < 6) {
-            if (items[i].quality < 50) {
-              items[i].quality = items[i].quality + 1
-            }
-          }
-        }
-      }
-    }
-    if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-      items[i].sell_in = items[i].sell_in - 1;
-    }
-    if (items[i].sell_in < 0) {
-      if (items[i].name != 'Aged Brie') {
-        if (items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-          if (items[i].quality > 0) {
-            if (items[i].name != 'Sulfuras, Hand of Ragnaros') {
-              items[i].quality = items[i].quality - 1
-            }
-          }
-        } else {
-          items[i].quality = items[i].quality - items[i].quality
-        }
-      } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
-        }
-      }
+      itemTypes['regular'].items.push(item);
     }
   }
-}
+};
+
+const decayQuality = (item, decayRate) => {
+  const { quality } = item;
+  let newQuality = quality - decayRate;
+
+  if (newQuality > 50) newQuality = 50;
+  if (newQuality < 0) newQuality = 0;
+
+  item.quality = newQuality;
+  item.sell_in -= 1;
+};
+
+const updateQuality = (item, type, sell_in, baseDecay) => {
+  let decayRate = baseDecay;
+
+  if (type === 'legendary') return;
+
+  if (type === 'pass') {
+    if (sell_in <= 0) {
+      return (item.quality = 0);
+    } else if (sell_in <= 5) {
+      decayRate = -3;
+    } else if (sell_in <= 10) {
+      decayRate = -2;
+    }
+  } else {
+    if (sell_in < 0) {
+      decayRate *= 2;
+    }
+  }
+
+  decayQuality(item, decayRate);
+};
+
+const updateItems = () => {
+  Object.keys(itemTypes).map(type => {
+    for (const item of itemTypes[type].items) {
+      updateQuality(item, type, item.sell_in, itemTypes[type].baseDecay);
+    }
+  });
+};
